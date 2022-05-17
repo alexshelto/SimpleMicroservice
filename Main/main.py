b0288206c1cda938aec0d10c0d63e2f2e1ae7af5
alexshelto <alexandershelton@icloud.com>
2022-05-17 14:01:58 -0400

    Typo and removing keyword id to pk

--- Main/main.py
+++ Main/main.py
@@ -1,13 +1,10 @@
-from __future__ import annotations
-
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemt import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
 
-# Connection to mysql db
 # sql://user:pwd@host(db from docker)/name from docker file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main_db'
 CORS(app)  # Frontend will need CORS
@@ -18,13 +15,13 @@ db = SQLAlchemy(app)
 # Product created in Django App (ADMIN)
 # This model will catch the event from rabbitMQ and create the product
 class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    pk = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
 
 class ProductUser(db.Model):
-    id = db.column(db.Integer, primary_key=True)
+    pk = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     product_id = db.Column(db.Integer)
 
