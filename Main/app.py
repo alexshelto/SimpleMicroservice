9de6ed7e1645ce9c01a311fb58de2fb3de18da22
alexshelto <alexandershelton@icloud.com>
2022-05-18 12:16:26 -0400

    Flask app communication with Admin

--- Main/app.py
+++ Main/app.py
@@ -1,4 +1,7 @@
+from dataclasses import dataclass
+
 from flask import Flask
+from flask import jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -18,12 +21,17 @@ migrate = Migrate(app, db)
 
 # Product created in Django App (ADMIN)
 # This model will catch the event from rabbitMQ and create the product
+@dataclass  # Allows to be jsonified
 class Product(db.Model):
+    pk: int
+    title: str
+    image: str
     pk = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
 
+@dataclass
 class ProductUser(db.Model):
     pk = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
@@ -33,9 +41,10 @@ class ProductUser(db.Model):
     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 
-@app.route('/')
+@app.route('/api/products')
 def index():
-    return 'hello'
+    print(Product.query.all())
+    return jsonify(Product.query.all())
 
 
 if __name__ == '__main__':
--- Main/consumer.py
+++ Main/consumer.py
@@ -1,5 +1,9 @@
 # pika used to send events
+import json
+
 import pika
+from app import db
+from app import Product
 
 
 params = pika.URLParameters(
@@ -14,10 +18,32 @@ channel.queue_declare(queue='main')
 
 def callback(channel, method, properties, body):
     print('received in main: ')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product = Product(
+            pk=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product created')
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product deleted')
 
 
-channel.basic_consume(queue='main', on_message_callback=callback)
+channel.basic_consume(
+    queue='main', on_message_callback=callback, auto_ack=True)
 
 print('started consuming')
 channel.start_consuming()
