dec3df6e203fd3640cedf9ba521c366c29556eab
alexshelto <alexandershelton@icloud.com>
2022-05-18 12:26:17 -0400

    Communication to Flask (Main)

--- admin/consumer.py
+++ admin/consumer.py
@@ -18,8 +18,21 @@ def callback(channel, method, properties, body):
     print(body)
 
 
-channel.basic_consume(queue='admin', on_message_callback=callback)
+channel.basic_consume(
+    queue='admin', on_message_callback=callback, auto_ack=True)
 
 print('started consuming')
 channel.start_consuming()
 channel.close()
+
+
+"""
+
+ADMIN:
+    consumer channel: "ADMIN"
+    producer channel: "MAIN"
+
+MAIN:
+    consumer channel: "MAIN"
+
+"""
--- admin/products/producer.py
+++ admin/products/producer.py
@@ -1,6 +1,6 @@
-# pika used to send events
-import pika
+import json
 
+import pika
 
 params = pika.URLParameters(
     'amqps://tzoklrjp:lDFUDWxOd19WnfvURom5DnVrUK8nhr_x@shrimp.rmq.cloudamqp.com/tzoklrjp',
@@ -11,5 +11,7 @@ connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
 
-def publish():
-    channel.basic_publish(exchange='', routing_key='main', body='hello',)
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main',
+                          body=json.dumps(body), properties=properties)
